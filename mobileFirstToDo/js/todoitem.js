1146cfbf21d96a1e0d91b0edda45f2f798fddd54
siran13hak <153432303+siran13hak@users.noreply.github.com>
2023-12-21 01:15:46 +0400

    Add files via upload

---
+++ mobileFirstToDo/js/todoitem.js
@@ -0,0 +1,19 @@
+export default class TodoItem {
+  constructor(){
+    this._id = null;
+    this._item = null
+  }
+  getId() {
+    return this._id;
+  }
+  setId(id) {
+    this._id = id
+  }
+
+  getItem() {
+    return this._item;
+  }
+  setItem(item) {
+    this._item = item
+  }
+}
